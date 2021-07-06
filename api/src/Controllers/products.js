7d3c90c5a516f37b090e9f196f6ad82b0d5611ac
estePecora <b.estefaniapecora@gmail.com>
2021-07-06 15:58:13 -0300

    ruta POST new Product added

---
+++ api/src/Controllers/products.js
@@ -0,0 +1,47 @@
+const { Sequelize } = require('sequelize')
+const { Product } = require('../db')
+
+
+const postNewProduct = async function postNewProduct (req, res) {
+    const { name, 
+            price, 
+            description,
+            weight, 
+            image, 
+            stock, 
+            type_product,
+            brand } = req.body;
+
+    const newProduct = await Product.findOrCreate({
+        where: {
+            name, 
+            price, 
+            description,
+            weight, 
+            image, 
+            stock, 
+            type_product
+
+        }
+
+    });
+    
+    let productBrand = await Brand.findAll({
+                where:{ 
+                    name: brand 
+                }
+            })
+            await newProduct[0].addBrand(productBrand)
+    
+
+    res.status(200).send({message: "Product added succesfully"})
+  
+};
+
+
+
+
+module.exports = {
+    postNewProduct,
+  
+};
--- api/src/routes/productsRoutes.js
+++ api/src/routes/productsRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 // const { getAllProducts } = require('../Controllers/products')
+const { postNewProduct } = require('../Controllers/products')
 const router = Router();
 
 
 // router.get('/', getAllProducts)
-
+router.post('/addProduct', postNewProduct)
 
 
 module.exports = router;
