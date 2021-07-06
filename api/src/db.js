5b5fbad55662efc43e27ff439178f1d797870566
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-06 13:39:16 -0300

    jorgeFer/add/Users to sequelize object model

--- api/src/db.js
+++ api/src/db.js
@@ -2,34 +2,40 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, 
-  native: false, 
-});
+const sequelize = new Sequelize(
+	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+	{
+		logging: false,
+		native: false,
+	}
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
+	.filter(
+		(file) =>
+			file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+	)
+	.forEach((file) => {
+		modelDefiners.push(require(path.join(__dirname, '/models', file)));
+	});
 
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+	entry[0][0].toUpperCase() + entry[0].slice(1),
+	entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Product } = sequelize.models;
-
+const { Product, Users } = sequelize.models;
 
 module.exports = {
-  ...sequelize.models, 
-  conn: sequelize,     
+	...sequelize.models,
+	conn: sequelize,
 };
