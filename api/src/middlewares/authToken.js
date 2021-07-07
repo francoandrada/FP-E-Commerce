e10a9ca5d9d0ba83a66951b7fe1ceb69bf6ad398
ana paula <salomoneanapaulah3@gmail.com>
2021-07-07 10:43:38 -0300

    update commits

--- api/src/middlewares/authToken.js
+++ api/src/middlewares/authToken.js
@@ -2,16 +2,19 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: '.env' });
 
 module.exports = (req, res, next) => {
+
 	const authHeader = req.get('Authorization');
 
 	if (authHeader) {
 		const token = authHeader.split(' ')[1];
+
 		try {
 			const user = jwt.verify(token, process.env.SECRET);
+			console.log(user);
 			req.user = user;
 		} catch (error) {
-            console.log('JWT no valido');
-			console.log(error);
+			console.log(error)
+           res.send('JWT no valido');
 
 		}
 	}
