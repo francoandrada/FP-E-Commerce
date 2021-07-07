afd4ecd20ef7325c428c51e46d708246d6dace1f
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-07 14:45:31 -0300

    jorgeFer/add/Searchbar file

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import logo from './logo.svg';
 import LogIn from './components/LogIn/LogIn';
 import './App.css';
-import { Route } from 'react-router'
+import { Route } from 'react-router';
 import Register from './components/Register/Register';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Link to='/login'><buttom>LogIn</buttom></Link>
-      <br />
-      <Link to='register' ><buttom>Register</buttom></Link>
-      <Route path='/LogIn' exact component={LogIn} />
-      <Route path="/register" exact component={Register} />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Link to='/login'>
+				<buttom>LogIn</buttom>
+			</Link>
+			<br />
+			<Link to='register'>
+				<buttom>Register</buttom>
+			</Link>
+			<Route path='/LogIn' exact component={LogIn} />
+			<Route path='/register' exact component={Register} />
+		</div>
+	);
 }
 
 export default App;
