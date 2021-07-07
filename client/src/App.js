682c21f9c9be289fd43eb24fb6703d253588ae6a
ana paula <salomoneanapaulah3@gmail.com>
2021-07-07 11:01:25 -0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import LogIn from './components/LogIn/LogIn';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <LogIn/>
-    </div>
+        
+  
+
   );
 }
 
--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,9 +1,15 @@
-// export default function rootReducer(state = initialState, action) {
-//   switch (action.type) {
+
+const initialState = {
+
+}
+
+
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
      
-//       default: {
-//           return state;
-//       }
-//   }
-// }
+      default: {
+          return state;
+      }
+  }
+}
 
