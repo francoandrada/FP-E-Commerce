f0dd8f932375e8d692fc20737deb45e5c96d99e4
Tomas Roquinaj <roquinaj.t@hotmail.com>
2021-07-08 12:46:21 -0300

    fixed(client/src/components/navbar) fixed import searchbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from '../Searchbar/SearchBar.jsx';
+import SearchBar from '../Searchbar/Searchbar';
 import './Navbar.css';
 
 const defaultOptions = [];
