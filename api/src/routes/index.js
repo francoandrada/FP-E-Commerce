ac0298ee06df5babb248a4d8957be7915d4b1a6b
estePecora <b.estefaniapecora@gmail.com>
2021-07-10 14:47:33 -0300

    Feat/Review route added

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,21 @@
 const { Router } = require('express');
 
-const productsRoutes = require('./productsRoutes')
-const usersRoutes = require('./usersRoutes')
-const authRoutes = require('./authRoutes')
-const adminRoutes = require('./adminRoutes')
+const productsRoutes = require('./productsRoutes');
+const usersRoutes = require('./usersRoutes');
+const authRoutes = require('./authRoutes');
+const adminRoutes = require('./adminRoutes');
 const brandRoutes = require('./brandRoutes');
 const authGmailRoutes = require('./authGmailRoutes');
-
+const reviewRoutes = require('./reviewRoutes');
 
 const router = Router();
 
-
-router.use('/products', productsRoutes )
-router.use('/users', usersRoutes )
-router.use('/auth', authRoutes )
-router.use('/authGmail',authGmailRoutes)
-router.use('/admin', adminRoutes)
+router.use('/products', productsRoutes);
+router.use('/users', usersRoutes);
+router.use('/auth', authRoutes);
+router.use('/authGmail', authGmailRoutes);
+router.use('/admin', adminRoutes);
 router.use('/brands', brandRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
