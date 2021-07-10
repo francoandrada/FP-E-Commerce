c4d08807bf35c25398b4c5ce8517fcd54075fa44
celenagonzalez <celena_sasha@hotmail.com>
2021-07-10 19:15:50 -0300

    filter

--- client/src/components/FilterCategories/FilterCategories.jsx
+++ client/src/components/FilterCategories/FilterCategories.jsx
@@ -1,11 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import styles from './Filter.module.css'
+function FilterCategories() {
+const categories= useSelector((state)=> state.category.allCategories)
+const Dispatch =useDispatch()
 
+// console.log(categories)
+useEffect({
 
-function FilterCate() {
-    return (
+})
+
+    return ( 
         <div>
-           
+            <h1 className={styles.div_celena}>Hola</h1>
+            {
+                categories.map((item, index)=>(
+                    <div className={styles.div_celena} key={index}>
+                        <p>{item.name}</p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
 
-export default Catalog;
+export default FilterCategories;
