3d4fc33f9f659032ede623fb4ea4d776e8fdd6f8
estePecora <b.estefaniapecora@gmail.com>
2021-07-10 17:55:28 -0300

    carrousel Home finish

--- client/src/components/ContainerComponents/Home/Home.jsx
+++ client/src/components/ContainerComponents/Home/Home.jsx
@@ -5,7 +5,6 @@ import styles from './Home.module.css'
 import BannerContainer from '../../BannerContainer/BannerContainer';
 import HighlightedProdContainer from '../HighlightedProdContainer/HighlightedProdContainer'
 import Footer from '../Footer/Footer'
-import Highlights from '../../Highlights/Highlights';
 
 
 function Home() {
@@ -21,7 +20,6 @@ function Home() {
                 <BannerContainer></BannerContainer>
                 <HighlightedProdContainer></HighlightedProdContainer>
             </div>
-            <Highlights></Highlights>
             
             <div>
                 <Footer></Footer>
