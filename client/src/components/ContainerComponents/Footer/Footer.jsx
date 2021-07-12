0daa2945fa93eab322c481da048071dcbef96c68
francoandrada <francoarganiaraz@gmail.com>
2021-07-12 02:05:27 -0300

    feat(/Footer /Products): little changes in styles

--- client/src/components/ContainerComponents/Footer/Footer.jsx
+++ client/src/components/ContainerComponents/Footer/Footer.jsx
@@ -1,19 +1,40 @@
-import './Footer.css'
+import './Footer.css';
+import styles from './Footer.module.css';
+import { AiFillGithub } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 function Footer() {
-
-    return(
-        <div className='FooterContainer'>
-            <div className='FooterInfo'> 
-                
-                <h1>Categories</h1>
-                <h1>About Us</h1>
-                <h1>Contact</h1>
-                
-            </div>
-           
-        </div>
-    )
+	return (
+		<div className={styles.footerContainer}>
+			<div className={styles.footerInfo}>
+				<div className={styles.logoContainer}>
+					<Link to='/'>
+						<h1 className={styles.logo}>HardwareStore</h1>
+					</Link>
+				</div>
+				<div className={styles.contentContainer}>
+					<Link to='/'>
+						<span>Home</span>
+					</Link>
+					&nbsp;
+					<Link to='/catalog'>
+						<span>Catalog</span>
+					</Link>
+				</div>
+				<div className={styles.contentContact}>
+					<div className={styles.containerGit}>
+						<a
+							href='https://github.com/francoandrada/FP-E-Commerce'
+							className={styles.gitLink}
+						>
+							<AiFillGithub className={styles.iconGit} />
+						</a>
+					</div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
--- client/src/components/ContainerComponents/Home/Home.jsx
+++ client/src/components/ContainerComponents/Home/Home.jsx
@@ -1,34 +1,30 @@
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { getHighlightProd } from '../../../Redux/actions'
-import styles from './Home.module.css'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getHighlightProd } from '../../../Redux/actions';
+import styles from './Home.module.css';
 import BannerContainer from '../../BannerContainer/BannerContainer';
-import HighlightedProdContainer from '../HighlightedProdContainer/HighlightedProdContainer'
-import Footer from '../Footer/Footer'
+import HighlightedProdContainer from '../HighlightedProdContainer/HighlightedProdContainer';
+import Footer from '../Footer/Footer';
 import AuthenticatedUser from '../../AuthUser.js/AuthenticatedUser';
 
-
 function Home() {
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getHighlightProd())    
-    }, [] )
+	useEffect(() => {
+		dispatch(getHighlightProd());
+	}, []);
 
-    return(
-    
-        <div className={styles.HomeContainer}>
-              <AuthenticatedUser />
-            <div> 
-                <BannerContainer></BannerContainer>
-                <HighlightedProdContainer></HighlightedProdContainer>
-            </div>
-            
-            <div>
-                <Footer></Footer>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.HomeContainer}>
+			<AuthenticatedUser />
+			<div>
+				<BannerContainer></BannerContainer>
+				<HighlightedProdContainer></HighlightedProdContainer>
+			</div>
+
+			<div>{/* <Footer></Footer> */}</div>
+		</div>
+	);
 }
 
 export default Home;
