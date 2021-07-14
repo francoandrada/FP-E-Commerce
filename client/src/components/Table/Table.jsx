f93e99a31c0f62791856a79ae1becaa4e6b58f6b
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-14 11:42:52 -0300

    jorgeFer/fix/(Table): I fixed the render of data

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getListOfProductTable } from '../../Redux/actions';
+import { useTable } from 'react-table';
+import { COLUMNS } from './columns';
 
 const Table = () => {
 	const dispatch = useDispatch();
@@ -15,9 +17,54 @@ const Table = () => {
 		dispatch(getListOfProductTable(0, test));
 	}, [dispatch]);
 
-	listProductsOnTable && console.log('mi lista', listProductsOnTable.products);
+	const columns = useMemo(() => COLUMNS, []);
+	const data = useMemo(
+		() => listProductsOnTable?.products,
+		[listProductsOnTable?.products]
+	);
 
-	return <h1>hola</h1>;
+	const tableInstance = useTable({
+		columns,
+		data: data || [],
+	});
+
+	const { getTableProps, getTableBodyProps, headerGroups, prepareRow, rows } =
+		tableInstance;
+	return (
+		<div>
+			{listProductsOnTable ? (
+				<table {...getTableProps()}>
+					<thead>
+						{headerGroups.map((headerGroup) => (
+							<tr {...headerGroup.getHeaderGroupProps()}>
+								{headerGroup.headers.map((column) => (
+									<th {...column.getHeaderProps()}>
+										{column.render('Header')}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody {...getTableBodyProps()}>
+						{rows.map((row) => {
+							prepareRow(row);
+							return (
+								<tr {...row.getRowProps()}>
+									{row.cells.map((cell) => {
+										return (
+											<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+										);
+									})}
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			) : (
+				<h1>Loading...</h1>
+			)}
+		</div>
+	);
 };
 
 export default Table;
