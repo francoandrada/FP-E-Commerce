830908e7aa51b996bf4631efe0d72f498aab526b
francoandrada <francoarganiaraz@gmail.com>
2021-07-15 21:13:36 -0300

    styles(/shoppingCart): add styles in shoppingCart

--- client/src/components/ShoppingCartComponents/CartProducts/CartProducts.jsx
+++ client/src/components/ShoppingCartComponents/CartProducts/CartProducts.jsx
@@ -1,59 +1,72 @@
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import {addToCart, removeFromCart, adjustQty} from '../../../Redux/actions'
-import style from './CartProducts.module.css'
-import {BsTrash} from 'react-icons/bs'
+import { addToCart, removeFromCart, adjustQty } from '../../../Redux/actions';
+import style from './CartProducts.module.css';
+import { BsTrash } from 'react-icons/bs';
+import { formatNumber } from '../../../helper/priceFormater';
 
-function CartList({info, image, name, price, qty}) {
-    const dispatch = useDispatch();
-    const [prodInfo, setProdInfo] = useState(info)
+function CartList({ info, image, name, price, qty }) {
+	const dispatch = useDispatch();
+	const [prodInfo, setProdInfo] = useState(info);
 
-    function handlePlus() {
-        setProdInfo(info)
-        dispatch(addToCart(prodInfo))
-    }
+	function handlePlus() {
+		setProdInfo(info);
+		dispatch(addToCart(prodInfo));
+	}
 
-    function handleMinus() {
-        let newQuantity = qty
-        if (qty === 1 ) {
-            newQuantity = qty
-        } else {
-            newQuantity = qty - 1
-        }
-        dispatch(adjustQty(info.id, newQuantity))
-    }
+	function handleMinus() {
+		let newQuantity = qty;
+		if (qty === 1) {
+			newQuantity = qty;
+		} else {
+			newQuantity = qty - 1;
+		}
+		dispatch(adjustQty(info.id, newQuantity));
+	}
 
-    function handleDeleteProd() {
-        dispatch(removeFromCart(info.id))
-    }
-	
+	function handleDeleteProd() {
+		dispatch(removeFromCart(info.id));
+	}
+
+	let subtot = price * qty;
+	let subtotFormat = formatNumber.new(subtot, '$');
+
 	return (
 		<div>
-        
-            <div className={style.shoppingCartContainer}>
-                <div className={style.cartProductsContainer}>
-                    <div className={style.cartImageDiv}>
-                        <img src={image} className={style.cartImage} alt='productFoto' />
-                    </div>
-                    <p>{name}</p>
-                    <h3>${price*qty}</h3>
-                
-                    <div className={style.modifyQtyButtons}>
-                        <button 
-                        onClick={handleMinus} 
-                        className={style.plusMinusButtons}>-</button>
-                        <h3>{qty}</h3>
-                        <button 
-                        onClick={handlePlus}
-                        className={style.plusMinusButtons}>+</button>
-                    </div>
-                
-                    <button onClick={handleDeleteProd} className={style.trashButtonCart}><BsTrash/></button>
-                </div>
-            </div>
+			<div className={style.shoppingCartContainer}>
+				<div className={style.cartProductsContainer}>
+					<div className={style.imgcontainerDiv}>
+						<div className={style.cartImageDiv}>
+							<img src={image} className={style.cartImage} alt='productFoto' />
+						</div>
+					</div>
+					<div className={style.prodName}>
+						<p>{name}</p>
+					</div>
+					<div className={style.subtot}>
+						<h3>{subtotFormat}</h3>
+					</div>
+					<div className={style.cantAndPrice}>
+						<div className={style.modifyQtyButtons}>
+							<button onClick={handleMinus} className={style.plusMinusButtons}>
+								-
+							</button>
+							<h3>{qty}</h3>
+							<button onClick={handlePlus} className={style.plusMinusButtons}>
+								+
+							</button>
+						</div>
+						<button
+							onClick={handleDeleteProd}
+							className={style.trashButtonCart}
+						>
+							<BsTrash />
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
-
 }
 
 export default CartList;
--- client/src/components/ShoppingCartComponents/SubTotal/SubTotal.jsx
+++ client/src/components/ShoppingCartComponents/SubTotal/SubTotal.jsx
@@ -1,57 +1,57 @@
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import {NavLink} from 'react-router-dom'
-import style from './SubTotal.module.css'
+import { NavLink } from 'react-router-dom';
+import style from './SubTotal.module.css';
+import { formatNumber } from '../../../helper/priceFormater';
 
+function SubTotal({ qty, userLogged }) {
+	const cartProducts = useSelector((state) => state.cart.cart);
+	const [totalPrice, setTotalPrice] = useState(0);
+	const [totalItems, setTotalItems] = useState(0);
 
-function SubTotal({qty, userLogged}) {
-    const cartProducts = useSelector((state) => state.cart.cart);
-    const [totalPrice, setTotalPrice] = useState(0);
-    const [totalItems, setTotalItems] = useState(0);
+	useEffect(() => {
+		let items = 0;
+		let price = 0;
 
-  useEffect(() => {
-    let items = 0;
-    let price = 0;
+		cartProducts &&
+			cartProducts.forEach((item) => {
+				items += item.qty;
+				price += item.qty * item.price;
+			});
 
-    cartProducts && cartProducts.forEach((item) => {
-      items += item.qty;
-      price += item.qty * item.price;
-    });
+		setTotalItems(items);
+		setTotalPrice(price);
+	}, [cartProducts, totalPrice, totalItems, setTotalPrice, setTotalItems]);
 
-    setTotalItems(items);
-    setTotalPrice(price);
-  }, [cartProducts, totalPrice, totalItems, setTotalPrice, setTotalItems]);
-
-   
-
+	let totalFormat = formatNumber.new(totalPrice, '$');
 	return (
 		<div>
-            <div className={style.subtotalContainerMain}>
-                <h4>ORDER SUMMARY</h4>
-                <div className={style.subdivTotal}>
-                    <p>Items in Cart:</p>
-                    <p>{totalItems}</p>
-                </div>
-                
-                <div className={style.subdivTotal}>
-                    <p>TOTAL:<br></br>(without shipping)</p>
-                    <h3>$ {totalPrice}</h3>
-                </div>
-                
-                
-                {userLogged ? 
-                    <NavLink to='/payment'>
-                    <button className={style.paymentButton}>Checkout</button>
-                    </NavLink> 
-                    : <NavLink to='/login'>
-                    <button className={style.paymentButton}>Checkout</button>
-                    </NavLink>
-                    }
-                
-            </div>
+			<div className={style.subtotalContainerMain}>
+				<h4>ORDER SUMMARY</h4>
+				<div className={style.subdivTotal}>
+					<p>Items in Cart:</p>
+					<p>{totalItems}</p>
+				</div>
+
+				<div className={style.subdivTotal}>
+					<p>
+						TOTAL:<br></br>(without shipping)
+					</p>
+					<h3>{totalFormat}</h3>
+				</div>
+
+				{userLogged ? (
+					<NavLink to='/payment'>
+						<button className={style.paymentButton}>Checkout</button>
+					</NavLink>
+				) : (
+					<NavLink to='/login'>
+						<button className={style.paymentButton}>Checkout</button>
+					</NavLink>
+				)}
+			</div>
 		</div>
 	);
-
 }
 
 export default SubTotal;
