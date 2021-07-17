cfe20aecaa081de49a8617514ef5ab335744deb7
francoandrada <francoarganiaraz@gmail.com>
2021-07-17 17:00:08 -0300

    add new elements

--- api/src/app.js
+++ api/src/app.js
@@ -23,7 +23,10 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header(
+		'Access-Control-Allow-Origin',
+		'https://ecommerceherni.herokuapp.com'
+	);
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header(
 		'Access-Control-Allow-Headers',
