c628507574309fa9ff3bfb5737a603e2690e91b4
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-17 11:41:12 -0300

    jorgeFer/add/the controller to count the categories

--- api/src/Controllers/admin/countCategories.controller.js
+++ api/src/Controllers/admin/countCategories.controller.js
@@ -1,7 +1,30 @@
+const { Product, Category } = require('../../db');
 //http://localhost:3001/admin/categoriescount
 
 const countCategories = async (req, res, next) => {
-	return res.json('hola categories');
+	const categories = await Category.findAll().then((response) => {
+		return response.map(({ name }) => name);
+	});
+
+	const productWithCategories = await Product.findAll({
+		include: { model: Category },
+	});
+
+	const counter = (array, categoryName) => {
+		return array.filter(({ categories: [{ name }] }) => name === categoryName)
+			.length;
+	};
+
+	const count = [];
+	let others = 0;
+	for (const category of categories) {
+		const n = counter(productWithCategories, category);
+		if (n !== 1) count.push({ [category]: n });
+		else others++;
+	}
+	count.push({ others });
+
+	return res.json(count);
 };
 
 module.exports = countCategories;
