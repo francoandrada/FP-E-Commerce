208c1e99b6a8229e2703c6ff3050a119bc324e04
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-07-18 18:10:28 -0300

    actualizando cambios para adecuar deploy/principal

--- client/src/Redux/actionsName.js
+++ client/src/Redux/actionsName.js
@@ -45,6 +45,10 @@ export const LOAD_CURRENT_ITEM = 'LOAD_CURRENT_ITEM';
 // pago
 export const SET_CART = 'SET_CART';
 
+//deploy
+export const SET_MANUAL_AUTHENTICATION = 'SET_MANUAL_AUTHENTICATION';
+
+
 /////////////////ADMIN
 // export const CREATED_BRAND= 'CREATED_BRAND';
 
