8bc747cea7f0181de6cfc1417b527b02c2b7877e
Tomas Roquinaj <roquinaj.t@hotmail.com>
2021-07-18 22:58:06 -0300

    Styles all components admin

--- client/src/components/AdminComponents/Table/Table.jsx
+++ client/src/components/AdminComponents/Table/Table.jsx
@@ -100,114 +100,117 @@ const Table = () => {
 
 	const paginate = (pageNumber) => dispatch(changeTablePage(pageNumber));
 	return (
-		<div className={styles.Container}>
-			<Admin />
-			<div className={styles.btnContainer}>
-				<Link to='/admin/addproduct'>
-					<button> Add Product </button>
-				</Link>
-			</div>
-			<div className= {styles.tableContainer}>
+		<div className={styles.background}>
+			<div className={styles.Container}>
+				<Admin />
+				<div className={styles.btnContainer}>
+					<Link to='/admin/addproduct'>
+						<button> Add Product </button>
+					</Link>
+				</div>
+				<div className={styles.tableContainer}>
 
-				<div
-					style={{
-						height: '100px',
-						background: 'white',
-						display: 'flex',
-						justifyContent: 'space-around',
-						alignItems: 'center',
-						width: '80%',
-						marginLeft: '20%',
-						borderRadius: '10px 10px 0 0',
-						flexDirection: 'column',
-					}}
-				>
-					<div className={styles.searchContainer}>
+					<div
+						style={{
+							height: '100px',
+							background: 'white',
+							display: 'flex',
+							justifyContent: 'space-around',
+							alignItems: 'center',
+							width: '918px',
+							marginLeft: '20%',
+							borderRadius: '10px 10px 0 0',
+							flexDirection: 'column',
+							boxShadow: '0 0px 30px rgb(172, 171, 171)'
+						}}
+					>
+						<div className={styles.searchContainer}>
 
-						<input
-							type='text'
-							value={searchFilter}
-							placeholder='Search...'
-							onChange={searchFilterHandle}
-						/>
-
-					</div>
-					<div className={styles.filterContainer}>
-
-						<Select
-							initialValue={sizePagination}
-							onChange={paginationSizeHandle}
-							values={[10, 20, 50, 100, 120]}
-						/>
-
-						<Select
-							initialValue={sortTable}
-							onChange={sortTableHandle}
-							values={['name', 'id', 'price', 'priceSpecial', 'weight', 'stock']}
-						/>
-
-						<Select
-							initialValue={orderTable}
-							onChange={orderTableHandle}
-							values={['default', 'ASC', 'DESC']}
-						/>
-
-						{allCategories && (
-							<Select
-								initialvalue={filterByCategory}
-								onChange={filterByCategoryHandle}
-								values={['default', ...allCategories.map(({ name }) => name)]}
+							<input
+								type='text'
+								value={searchFilter}
+								placeholder='Search...'
+								onChange={searchFilterHandle}
 							/>
-						)}
 
-						{allBrands && (
+						</div>
+						<div className={styles.filterContainer}>
+
 							<Select
-								initialValue={tableByBrand}
-								onChange={filterByBrandHandle}
-								values={['default', ...allBrands.map(({ name }) => name)]}
+								initialValue={sizePagination}
+								onChange={paginationSizeHandle}
+								values={[10, 20, 50, 100, 120]}
 							/>
-						)}
+
+							<Select
+								initialValue={sortTable}
+								onChange={sortTableHandle}
+								values={['name', 'id', 'price', 'priceSpecial', 'weight', 'stock']}
+							/>
+
+							<Select
+								initialValue={orderTable}
+								onChange={orderTableHandle}
+								values={['default', 'ASC', 'DESC']}
+							/>
+
+							{allCategories && (
+								<Select
+									initialvalue={filterByCategory}
+									onChange={filterByCategoryHandle}
+									values={['default', ...allCategories.map(({ name }) => name)]}
+								/>
+							)}
+
+							{allBrands && (
+								<Select
+									initialValue={tableByBrand}
+									onChange={filterByBrandHandle}
+									values={['default', ...allBrands.map(({ name }) => name)]}
+								/>
+							)}
+						</div>
 					</div>
 				</div>
-			</div>
-			{listProductsOnTable ? (
-				<table {...getTableProps()} className={styles.tableEcommerce}>
-					<thead>
-						{headerGroups.map((headerGroup) => (
-							<tr {...headerGroup.getHeaderGroupProps()}>
-								{headerGroup.headers.map((column) => (
-									<th {...column.getHeaderProps()}>
-										{column.render('Header')}
-									</th>
-								))}
-							</tr>
-						))}
-					</thead>
-					<tbody {...getTableBodyProps()}>
-						{rows.map((row) => {
-							prepareRow(row);
-							return (
-								<tr {...row.getRowProps()}>
-									{row.cells.map((cell) => {
-										return (
-											<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-										);
-									})}
+				{listProductsOnTable ? (
+					<table {...getTableProps()} className={styles.tableEcommerce}>
+						<thead>
+							{headerGroups.map((headerGroup) => (
+								<tr {...headerGroup.getHeaderGroupProps()}>
+									{headerGroup.headers.map((column) => (
+										<th {...column.getHeaderProps()}>
+											{column.render('Header')}
+										</th>
+									))}
 								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-			) : (
-				<Loader />
-			)}
+							))}
+						</thead>
+						<tbody {...getTableBodyProps()}>
+							{rows.map((row) => {
+								prepareRow(row);
+								return (
+									<tr {...row.getRowProps()}>
+										{row.cells.map((cell) => {
+											return (
+												<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+											);
+										})}
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				) : (
+					<Loader />
+				)}
 
-			{listProductsOnTable && listProductsOnTable?.totalPages > 1 && (
-				<PaginationTable
-					totalPages={listProductsOnTable?.totalPages}
-					paginate={paginate}
-				/>
-			)}
+				{listProductsOnTable && listProductsOnTable?.totalPages > 1 && (
+					<PaginationTable
+						totalPages={listProductsOnTable?.totalPages}
+						paginate={paginate}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
