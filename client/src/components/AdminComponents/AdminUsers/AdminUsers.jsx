e27443214f94a66905a0ec800b8056300f7509df
francoandrada <francoarganiaraz@gmail.com>
2021-07-19 15:23:38 -0300

    refactor(adminUsers): table structure

--- client/src/components/AdminComponents/AdminUsers/AdminUsers.jsx
+++ client/src/components/AdminComponents/AdminUsers/AdminUsers.jsx
@@ -10,6 +10,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import SearchBox from './SearchBox';
 import stylesAdmin from './AdminUser.module.css';
+import { FaTrashAlt } from 'react-icons/fa';
 
 function AdminUsers() {
 	const history = useHistory();
@@ -20,6 +21,11 @@ function AdminUsers() {
 	useEffect(() => {
 		dispatch(getUsers());
 	}, []);
+
+	let acu = 0;
+	const sum = () => {
+		return (acu += 1);
+	};
 
 	const handleDelete = async (email) => {
 		try {
@@ -73,27 +79,41 @@ function AdminUsers() {
 				<div className={styles.SearchBoxContainer}>
 					<SearchBox filter={filter} setFilter={setFilter} />
 				</div>
-				<div className={styles.UserContainer}>
+				<div className={styles.userContainer}>
 					<h1> Users </h1>
-					{users?.map((c) => {
-						return (
-							<div className={styles.category} key={c.email}>
-								<span>User: {c.email}</span>
-								<div>
-									<Link to={`/admin/user/${c.email}`}>
-										<button className={styles.btnEdit}>
-											<MdModeEdit />
-										</button>
-									</Link>
-								</div>
-								<div>
-									<button className={styles.btnEdit}>
-										<MdDelete onClick={() => onDeleteClick(c.email)} />
-									</button>
-								</div>
-							</div>
-						);
-					})}
+					<table class='table'>
+						<thead>
+							<tr>
+								<th class='col-md-1'>#</th>
+								<th>Name</th>
+								<th>Actions</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							{users?.map((c) => {
+								return (
+									<tr>
+										<th>{sum()}</th>
+										<td>{c.email}</td>
+										<td>
+											<div>
+												<Link key={c.id} to={`/admin/user/${c.email}`}>
+													<button className={styles.btnEdit}>
+														<MdModeEdit />
+													</button>
+												</Link>
+												<button onClick={() => onDeleteClick(c.email)}>
+													<FaTrashAlt />
+												</button>
+											</div>
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+					;
 				</div>
 			</div>
 		</div>
