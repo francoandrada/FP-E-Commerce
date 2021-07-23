f5420ec2de0cc43edf9994d46d2f34e46941cc2f
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-23 10:13:34 -0300

    jorgeFer(getOrders.controller):add/query to do a search

--- api/src/Controllers/admin/getOrders.controller.js
+++ api/src/Controllers/admin/getOrders.controller.js
@@ -1,20 +1,42 @@
+const { Op } = require('sequelize');
 const { OrderDetail, Product } = require('../../db');
 
 // http://localhost:3001/admin/listorders?page=1
 /*
 	body {
-		limit: int (size of the page)
+		limit: int (size of the page),
+		search: ""
 	}
 */
 
 const getOrders = async (req, res, next) => {
 	let products = {};
-	const { limit } = req.body;
+	const { limit, search } = req.body;
 	const pageAsNumber = Number.parseInt(req.query.page);
 	const limitToNumber = Number.parseInt(limit);
 
 	let page = 0;
 	if (!Number.isNaN(pageAsNumber) && pageAsNumber >= 0) page = pageAsNumber;
+
+	if (search && search.trim()) {
+		products = await OrderDetail.findAndCountAll({
+			limit: limitToNumber,
+			offset: page * limitToNumber,
+			include: {
+				model: Product,
+				where: {
+					name: {
+						[Op.iLike]: `%${search.toLowerCase()}%`,
+					},
+				},
+			},
+		});
+
+		return res.json({
+			totalPages: Math.floor(products.count / limitToNumber),
+			products: products.rows,
+		});
+	}
 
 	products = await OrderDetail.findAndCountAll({
 		limit: limitToNumber,
