1cdc2e18be3103e3bdef412c140bf44128a283c0
HELL0ANTHONY <george.anthony100@hotmail.com>
2021-07-23 11:14:53 -0300

    jorgeFer(UserOrder):add/I added an input for the search

--- client/src/components/AdminComponents/UserOrder/UserOrder.js
+++ client/src/components/AdminComponents/UserOrder/UserOrder.js
@@ -1,55 +1,39 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-	getUserWithOrdersDetail,
-	changeTableOrderUserPaginationSize,
-} from '../../../Redux/actions';
+import { getUserWithOrdersDetail } from '../../../Redux/actions';
 import COLUMNS from './columns';
 import Loader from '../../Loader/Loader';
 import Table from '../TableComponent/TableComponent';
 import Select from '../../Select/Select';
+import UserOrderLogic from './UserOrderLogic';
 
 const UserOrder = () => {
 	const dispatch = useDispatch();
+	const [searchValue, setSearchValue] = React.useState('');
+	const { mapData, changePaginationSizeHandle } = UserOrderLogic();
 	const { userWithOrder, tableOrderUserPaginationSize } = useSelector(
 		(state) => state.admin
 	);
 
 	React.useEffect(() => {
 		dispatch(
-			getUserWithOrdersDetail(0, { limit: tableOrderUserPaginationSize })
+			getUserWithOrdersDetail(0, {
+				limit: tableOrderUserPaginationSize,
+				search: searchValue,
+			})
 		);
-	}, [dispatch, tableOrderUserPaginationSize]);
+	}, [dispatch, tableOrderUserPaginationSize, searchValue]);
 
-	const mapData = (array) => {
-		const data =
-			array &&
-			array.map((o) => {
-				return {
-					orderId: o?.orderId || '--',
-					date: o?.createdAt || '--',
-					updatedAt: o?.updatedAt || '--',
-					status: o?.status || '--',
-					ammount: o?.ammount || '--',
-					userName: o?.user?.name || '--',
-					email: o?.user.email || '--',
-					surname: o?.user?.surname || '--',
-					userId: o?.user?.userId || '--',
-				};
-			});
-		return data;
-	};
-
-	const dataToPrint = mapData(userWithOrder?.products);
-
-	const changePaginationSizeHandle = (event) => {
+	const searchHandle = (event) => {
 		event.preventDefault();
-		dispatch(changeTableOrderUserPaginationSize(event.target.value));
+		setSearchValue(event.target.value);
 	};
 
 	return (
 		<div>
 			<div>
+				<input type='text' value={searchValue} onChange={searchHandle} />
+
 				<Select
 					initialValue={tableOrderUserPaginationSize}
 					onChange={changePaginationSizeHandle}
@@ -57,7 +41,10 @@ const UserOrder = () => {
 				/>
 			</div>
 			{userWithOrder ? (
-				<Table dataToPrint={dataToPrint} formatColumn={COLUMNS} />
+				<Table
+					dataToPrint={mapData(userWithOrder?.products)}
+					formatColumn={COLUMNS}
+				/>
 			) : (
 				<Loader />
 			)}
