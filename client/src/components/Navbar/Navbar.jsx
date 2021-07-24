2cb27938a23cfd946136627088c8d55f3340e507
estePecora <b.estefaniapecora@gmail.com>
2021-07-24 17:11:36 -0300

    fix/navBarStyle

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -220,6 +220,8 @@ const Navbar = () => {
 				<div className={styles.sections}>
 					<Link to='/'>Home</Link>
 					<Link to='/catalog'>Catalog</Link>
+					<Link to='/about'>About</Link>
+					<Link to='/about'>Contact</Link>
 				</div>
 			</div>
 		</>
