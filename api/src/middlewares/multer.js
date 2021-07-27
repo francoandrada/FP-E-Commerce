d9de7ef616428a824878f0624ecb0e8598af46d3
francoandrada <francoarganiaraz@gmail.com>
2021-07-27 16:25:39 -0300

    feat(): add little changes

--- api/src/middlewares/multer.js
+++ api/src/middlewares/multer.js
@@ -3,7 +3,18 @@ var multer = require('multer');
 const storage = multer.memoryStorage();
 //multer({ storage }).single('image');establece la opción de almacenamiento que usaremos en la aplicación, mientras que .single('image');especifica el nombre del campo al que multerdebe ir cuando busca el archivo.
 //const multerUploads = multer({ storage }).single('image');
-const multerUploads = multer({ storage }).array('images');
+const multerUploads = multer({
+	storage,
+	fileFilter: (req, file, cb) => {
+		const filetypes = /jpeg|jpg|png/;
+		const mimetype = filetypes.test(file.mimetype);
+		const extname = filetypes.test(path.extname(file.originalname));
+		if (mimetype && extname) {
+			return cb(null, true);
+		}
+		cb('Error: the file must be a valid image');
+	},
+}).array('images', 6);
 
 module.exports = {
 	multerUploads,
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -8,12 +8,12 @@ import {
 	addToCart,
 	removeFromCart,
 } from '../../Redux/actions';
-import FavoriteButton from '../FavoriteComponent/FavoriteButton'
+import FavoriteButton from '../FavoriteComponent/FavoriteButton';
 import { Link } from 'react-router-dom';
 import styles from './Products.module.css';
 import PagingBox from '../PagingBox/PagingBox';
 import ButtonCrypto from '../StyledComponents/ButtonCrypto';
-import './index.css'
+import './index.css';
 function Products() {
 	const arsBtc = useSelector((state) => state.crypto.arsBtc);
 	const rateUpdateTime = useSelector((state) => state.crypto.updateTime);
@@ -130,7 +130,6 @@ function Products() {
 	};
 	return (
 		<div className={styles.cardsContainer}>
-			
 			{productsToRender
 				? productsToRender.map((p) => {
 						if (p.name.length > 55) {
@@ -153,33 +152,33 @@ function Products() {
 								<div>
 									<hr id={styles.line} />
 								</div>
-								
+
 								<div className={styles.data}>
 									<span className={styles.productName}>{p.name}</span>
 									<div className={styles.heartDiv}>
-									<FavoriteButton prod={p}/>
+										<FavoriteButton prod={p} />
 									</div>
 								</div>
 								<div className={styles.footerCard}>
-								<div className='footerCard d-flex justify-content-center'>
-									<div className={styles.productPrice}>
-										<span>{formatPrice}</span>
-									</div>
+									<div className='footerCard d-flex justify-content-center'>
+										<div className={styles.productPrice}>
+											<span>{formatPrice}</span>
+										</div>
 
-									<div className={styles.buttonBuy}>
-										{p.stock > 0 ? (
-											<button
-												id={styles.btnBuy}
-												type='submit'
-												onClick={() => dispatch(addToCart(p))}
-											>
-												Add to Cart
-											</button>
-										) : (
-											<button type='submit'>Sin Stock</button>
-										)}
+										<div className={styles.buttonBuy}>
+											{p.stock > 0 ? (
+												<button
+													id={styles.btnBuy}
+													type='submit'
+													onClick={() => dispatch(addToCart(p))}
+												>
+													Add to Cart
+												</button>
+											) : (
+												<button type='submit'>Sin Stock</button>
+											)}
+										</div>
 									</div>
-								</div>
 								</div>
 								<div id={styles.paginado}></div>
 							</div>
